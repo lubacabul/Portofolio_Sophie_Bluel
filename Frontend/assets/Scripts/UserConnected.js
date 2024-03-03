1a96bacd34ce4318129a5b852f46e1a86c40e976
Karine Leclavier <lubacabul@gmail.com>
2024-03-03 14:40:40 +0100

    Ajout/suppression des élèments selon l'état d'authentification de l'utilisateur fonctionnel

--- Frontend/assets/Scripts/UserConnected.js
+++ Frontend/assets/Scripts/UserConnected.js
@@ -1,28 +1,66 @@
-// Création de la fonction pour vérifier si l'utilisateur est connecté
+//-------------FONCTION ADMINISTRATION-------------//
+
 async function checkUserConnected () {
     const token = localStorage.getItem('token');
-    /* Vérification présence token
-       console.log(token);
-    */
+ 
    const userConnected = token != null && token != undefined && token != '';
 
+   //--Si l'utilisateur est connecté--//
    if (userConnected) {
-      // Si l'utilisateur est connecté 
-      const loginLink = document.querySelector(".login__link");
-      loginLink.textContent = "logout";
-      loginLink.addEventListener("click", userLogOut);
+      
+            //Affichage des éléments : barre d'édition, bouton "modifier" et suppression des filtres//
+            const loginLink = document.querySelector(".login__link");
+            loginLink.textContent = "logout";
+            loginLink.addEventListener("click", userLogOut);
+            
+            blackBanner()
+            
+            const buttonModify = document.querySelector(".buttonModify");
+            buttonModify.style.display = 'block';
 
-   } else {
-      // Si l'utilisateur est déconnecté
-      const loginLink = document.querySelector(".login__link");
-      loginLink.textContent = "login";
-   }
+            const filtersSection = document.querySelector(".filter-container");
+            filtersSection.style.display = 'none';
+
+    // Si l'utilisateur est déconnecté //
+    } else {
+      
+            //Suppression des élèments ajoutés pour la fonction admin//
+            const loginLink = document.querySelector(".login__link");
+            loginLink.textContent = "login";
+
+            const buttonModify = document.querySelector(".buttonModify");
+            buttonModify.style.display = 'none';
+        }
 }
 
-// Fonction de déconnexion
-async function userLogOut() {
-   // Nettoyage du localStorage
-   localStorage.clear();
-   // Rechargement de la page 
-   window.location.reload();
+//Affichage du bandeau noir pour le mode édition//
+function blackBanner() {
+    const header = document.querySelector("header");
+    const displayBlackBanner = document.createElement("div");
+    header.appendChild(displayBlackBanner);
+    displayBlackBanner.innerHTML = `
+    <aside
+      id="blackBanner"
+      class="blackBanner"
+      aria-hidden="true"
+      role="dialog"
+      aria-modal="false"
+    >
+      <div>
+        <p> <i class="fa-regular fa-pen-to-square"></i> Mode édition </p>
+      </div>
+    </aside>
+    `;
 }
+
+            // Fonction de déconnexion //
+            async function userLogOut() {
+
+            // Nettoyage du localStorage
+            localStorage.clear();
+
+            // Rechargement de la page 
+            window.location.reload();
+    }
+
+
--- Frontend/assets/Scripts/index.js
+++ Frontend/assets/Scripts/index.js
@@ -1,4 +1,5 @@
-//-------------Récupération des API-------------//
+//-------------RECUPERATION DES API-------------//
+
 const gallery = document.querySelector(".gallery");
 const portfolio = document.querySelector("portfolio")
 let works = [];
@@ -25,53 +26,54 @@ function categoriesImport() {
 categoriesImport();
 
 //----------AFFICHAGE DES CATEGORIES ET PROJETS----------//
-//--Création de la gallerie de travaux--//
+    //--Création de la gallerie de travaux--//
 
-function generateWorks(worksArray) {
-    gallery.innerHTML = "";
+    function generateWorks(worksArray) {
+        gallery.innerHTML = "";
 
-    worksArray.forEach((work) => {
-        //Création des élèments des figures (l'image et le titre de l'image)
-        const figure = document.createElement("figure");
-        gallery.appendChild(figure);
-        figure.classList = work.category.name;
-        figure.setAttribute("data-id", work.id);
+        worksArray.forEach((work) => {
 
-        const img = document.createElement("img");
-        img.src = work.imageUrl;
-        img.alt = work.title;
-        figure.appendChild(img);
+                //Création des élèments des figures (l'image et le titre de l'image)
+                const figure = document.createElement("figure");
+                gallery.appendChild(figure);
+                figure.classList = work.category.name;
+                figure.setAttribute("data-id", work.id);
 
-        const figcaption = document.createElement("figcaption");
-        figcaption.innerHTML = work.title;
-        figure.appendChild(figcaption);
-    });
-}
+                const img = document.createElement("img");
+                img.src = work.imageUrl;
+                img.alt = work.title;
+                figure.appendChild(img);
 
-//-- Fonction de filtrage selon les catégories--//
-const filters = document.querySelectorAll(".filter");
+                const figcaption = document.createElement("figcaption");
+                figcaption.innerHTML = work.title;
+                figure.appendChild(figcaption);
+            });
+        }
 
-//Selection de la div concernée pour les filtres
-function worksFilter() {
-    filters.forEach((filter) => {
-        const filterValue = filter.textContent;
+    //-- Fonction de filtrage selon les catégories--//
+    const filters = document.querySelectorAll(".filter");
 
-        
-        filter.addEventListener("click", () => {
-            let filteredWorks = [];
+                //Selection de la div concernée pour les filtres//
+                function worksFilter() {
+                    filters.forEach((filter) => {
+                        const filterValue = filter.textContent;
 
-            //Création du filtre "Tous"
-            if (filterValue === "Tous") {
-                filteredWorks = works;
+                        
+                        filter.addEventListener("click", () => {
+                            let filteredWorks = [];
 
-            //Création des autres filtres
-            } else {
-                filteredWorks = works.filter(
-                    (work) => work.category.name === filterValue
-                );
-            }
-            generateWorks(filteredWorks);
-        });
-    });
-}
-worksFilter();
+                            //Création du filtre "Tous"
+                            if (filterValue === "Tous") {
+                                filteredWorks = works;
+
+                            //Création des autres filtres
+                            } else {
+                                filteredWorks = works.filter(
+                                    (work) => work.category.name === filterValue
+                                );
+                            }
+                            generateWorks(filteredWorks);
+                        });
+                    });
+                }
+                worksFilter();
